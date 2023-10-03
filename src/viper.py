cb132b1f0f7783c714a4e6537b7f4c032052b6a1
Matthieu Medeng <94767386+medengessia@users.noreply.github.com>
2023-10-03 16:08:49 +0200

    fix: some corrections for the viper module

--- src/viper.py
+++ src/viper.py
@@ -1,19 +1,24 @@
 import numpy as np
 
 
-def get_policy_nn(mdp, algo_rl, n_iter):
+def get_policy_nn(mdp, algo_rl, n_iter, path_to_expert=None):
     """Generates a RL policy by training a RL Neural Network and returns the policy.
 
     Args:
         mdp (gym.Env): a Markov Decision Process (Reinforcement Learning Problem)
-        algo_rl (abc.ABCMeta): A RL Algorithm
-        n_iter (int): Number of exploration iterations
+        algo_rl : A RL Algorithm
+        n_iter (int): Number of iterations
+        path_to_expert (str): a path to an existing policy. Defaults to None.
 
     Returns:
-        stable_baselines3.dqn.dqn.DQN: The obtained policy
+        The obtained policy.
     """
     policy = algo_rl('MlpPolicy', mdp, verbose=1)
-    policy.learn(n_iter) # Training of the policy
+
+    if path_to_expert is not None:
+        policy = algo_rl.load(path_to_expert)
+    else:
+        policy.learn(n_iter) # Training of the policy
 
     return policy
 
@@ -22,26 +27,33 @@ def generate_data(mdp, policy, n_iter):
     """Generates data, with actions weighted by their reward.
 
     Args:
-        policy (stable_baselines3.dqn.dqn.DQN): A policy
-        n_iter (int): Number of exploration iterations
+        mdp (gym.Env): a Markov Decision Process (Reinforcement Learning Problem)
+        policy : A RL policy
+        n_iter (int): Number of iterations
 
     Returns:
         tuple: The obtained dataset
     """
     s, _ = mdp.reset()
-    X, y = np.zeros((n_iter, mdp.observation_space.shape[0])), np.zeros(n_iter) # Initialization of the dataset
+    X, y, rewards = np.zeros((n_iter, mdp.observation_space.shape[0])), np.zeros(n_iter), np.zeros(n_iter) # Initialization of the dataset
+    r_T = 0    # The computed reward for one trajectory
 
+    start = 0  # Initialization of the first index with a particular reward
     for i in range(n_iter):
         X[i] = s   # Current state
         action, _ = policy.predict(s, deterministic=True)
         new_s, reward, terminated, truncated, infos = mdp.step(action) # Moving to a new state
         s = new_s
-        y[i] = action*reward   # The chosen action, weighted by its reward
+        y[i] = action   # The chosen action
+        r_T += reward
 
-        if terminated or truncated:
+        if terminated or truncated:  # At this point, r_T is the cumulated reward along the episode
             s, _ = mdp.reset()
+            rewards[start:i+1] = r_T
+            start = i + 1   # The variable start takes the value of the next index
+            r_T = 0 
 
-    return X, y
+    return X, y, rewards
 
 
 def fit_dt(data, algo_dt, depth=5):
@@ -49,29 +61,30 @@ def fit_dt(data, algo_dt, depth=5):
 
     Args:
         data (tuple): The dataset to fit
-        algo_dt (abc.ABCMeta): A decision tree algorithm
+        algo_dt : A decision tree algorithm
         depth (int): The desired maximal depth. Defaults to 5.
 
     Returns:
-        sklearn.tree._classes.DecisionTreeClassifier: The trained classifier
+        The trained decicision tree.
     """
-    X, y = data[0], data[1] # Extracting the states as input features and the actions as labels
+    X, y, rewards = data[0], data[1], data[2] # Extracting the states as input features and the actions as labels
     d_tree = algo_dt(max_depth=depth)
     d_tree.fit(X,y) # Training of the decision tree 
+    print("Score of fitted DT is {}".format(d_tree.score(X,y)))
 
     return d_tree
 
 
-def choose_best_dt(dt_list, mdp, n_iter=10_000):
+def choose_best_dt(dt_list, mdp, n_iter=5_000):
     """Returns the decision tree with the best reward.
 
     Args:
         dt_list (list): A list of decision trees
         mdp (gym.Env): A Markov Decision Process (Reinforcement Learning Problem)
-        n_iter (int): Number of exploration iterations. Defaults to 10_000.
+        n_iter (int): Number of exploration iterations. Defaults to 5_000.
 
     Returns:
-        sklearn.tree._classes.DecisionTreeClassifier: The best decision tree.
+        The best decision tree.
     """
     best_tree = dt_list[0]
     best_reward = 0
@@ -93,31 +106,32 @@ def choose_best_dt(dt_list, mdp, n_iter=10_000):
         
         if sum_rewards > best_reward: # Updating the best reward and the best tree
             best_reward = sum_rewards
+            print("New best reward is {}".format(best_reward))
             best_tree = dt
         
     return best_tree
 
 
-def Viper(mdp, algo_dt, algo_rl, n_iter_exploit, n_iter_explore):
+def Viper(mdp, algo_dt, algo_rl, iter_viper, nb_data_from_nnpolicy, path_to_expert=None):
     """Proposes an implementation of Viper algorithm.
 
     Args:
         mdp (gym.Env): A Markov Decision Process (Reinforcement Learning Problem)
-        algo_dt (abc.ABCMeta): A decision tree algorithm
-        algo_rl (abc.ABCMeta): A RL Algorithm
-        n_iter_exploit (int): Number of exploitation iterations
-        n_iter_explore (int): Number of exploration iterations
+        algo_dt : A decision tree algorithm
+        algo_rl : A RL Algorithm
+        iter_viper (int): Number of iterations for VIPER
+        nb_data_from_nnpolicy (int): Number of iterations for data generating functions
+        path_to_expert (str): a path to an existing policy. Defaults to None.
 
     Returns:
-        sklearn.tree._classes.DecisionTreeClassifier: The best decision tree to evaluate a policy.
+        The best decision tree to evaluate a policy.
     """
-
-    policy = get_policy_nn(algo_rl, mdp, n_iter_explore)
+    policy = get_policy_nn(mdp, algo_rl, nb_data_from_nnpolicy, path_to_expert)
     dt_list = []
 
-    for i in range(n_iter_exploit):
-
-        data = generate_data(policy, mdp, n_iter_explore)
+    for i in range(iter_viper):
+        print('iteration {}'.format(i))
+        data = generate_data(mdp, policy, nb_data_from_nnpolicy)
         dt = fit_dt(data, algo_dt)
         dt_list.append(dt)
 
