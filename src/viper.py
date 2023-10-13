82bf82a3d72d98b04d25774648f58abab1e4a226
Matthieu Medeng <94767386+medengessia@users.noreply.github.com>
2023-10-13 03:21:22 +0200

    feat: adding function
    Adding function for getting data from a list of datasets.

--- src/viper.py
+++ src/viper.py
@@ -5,9 +5,9 @@ def get_policy_nn(mdp, algo_rl, n_iter, path_to_expert=None):
     """Generates a RL policy by training a RL Neural Network and returns the policy.
 
     Args:
-        mdp (gym.Env): a Markov Decision Process (Reinforcement Learning Problem)
-        algo_rl : A RL Algorithm
-        n_iter (int): Number of iterations
+        mdp (gym.Env): a Markov Decision Process (Reinforcement Learning Problem).
+        algo_rl : A RL Algorithm.
+        n_iter (int): Number of iterations.
         path_to_expert (str): a path to an existing policy. Defaults to None.
 
     Returns:
@@ -23,35 +23,30 @@ def get_policy_nn(mdp, algo_rl, n_iter, path_to_expert=None):
     return policy
 
 
-def generate_data(mdp, reward_weight, policy, n_iter):
+def generate_data(mdp, policy, n_iter):
     """Generates data, with actions weighted by their reward.
 
     Args:
-        mdp (gym.Env): a Markov Decision Process (Reinforcement Learning Problem)
-        reward_weight (float): Weight to control the influence of rewards on action selection
-        policy : A RL policy
-        n_iter (int): Number of iterations
+        mdp (gym.Env): a Markov Decision Process (Reinforcement Learning Problem).
+        policy : A RL policy.
+        n_iter (int): Number of iterations.
 
     Returns:
-        tuple: The obtained dataset
+        Ndarray: The obtained dataset.
     """
     s, _ = mdp.reset()
-    X, y, rewards = np.zeros((n_iter, mdp.observation_space.shape[0])), np.zeros(n_iter), np.zeros(n_iter) # Initialization of the dataset
+    X, y, rewards = np.zeros((n_iter, mdp.observation_space.shape[0])), np.zeros((n_iter,1)), np.zeros((n_iter,1)) # Initialization of the dataset
     r_T = 0    # The computed reward for one trajectory
 
     start = 0  # Initialization of the first index with a particular reward
     for i in range(n_iter):
         X[i] = s   # Current state
 
-        # Calculate the action probabilities based on rewards
-        action_values, _ = policy.predict(s, deterministic=False) # Use stochastic action selection
-        action_probs = np.exp(reward_weight * action_values) / np.sum(np.exp(reward_weight * action_values))
+        action, _ = policy.predict(s, deterministic=True) # Entering a state
 
-        # Choose an action according to the action probabilities
-        action = np.random.choice(policy.action_space.n, p=action_probs)
+        new_s, reward, terminated, truncated, infos = mdp.step(action) # Taking action
+        s = new_s # Getting a new state
 
-        new_s, reward, terminated, truncated, infos = mdp.step(action) # Moving to a new state
-        s = new_s
         y[i] = action   # The chosen action
         r_T += reward
 
@@ -61,21 +56,41 @@ def generate_data(mdp, reward_weight, policy, n_iter):
             start = i + 1   # The variable start takes the value of the next index
             r_T = 0 
 
-    return X, y, rewards
+    dataset = np.hstack((X,y))
+    dataset = np.hstack((dataset,rewards))
+
+    return dataset
+
+
+def get_data_from_datasets(datasets):
+    """Extracts a dataset for training a decision tree from a list of datasets based on their biggest reward,
+    with respect to a probability distribution.
+
+    Args:
+        datasets (list): A list of datasets.
+
+    Returns:
+        Ndarray: The chosen dataset.
+    """
+    sum_max = np.sum([np.amax(datasets[i][2]) for i in range(len(datasets))])
+    distributions = [np.amax(datasets[i][2])/sum_max for i in range(len(datasets))]
+    dataset_dt = np.random.choice(datasets, 1, p=distributions)
+
+    return dataset_dt
 
 
 def fit_dt(data, algo_dt, depth=5):
     """Fits a decision tree to the policy-generated dataset.
 
     Args:
-        data (tuple): The dataset to fit
-        algo_dt : A decision tree algorithm
+        data (Ndarray): The dataset to fit.
+        algo_dt : A decision tree algorithm.
         depth (int): The desired maximal depth. Defaults to 5.
 
     Returns:
         The trained decicision tree.
     """
-    X, y, rewards = data[0], data[1], data[2] # Extracting the states as input features and the actions as labels
+    X, y, rewards = data[:,0], data[:,1], data[:,2] # Extracting the states as input features and the actions as labels
     d_tree = algo_dt(max_depth=depth)
     d_tree.fit(X,y) # Training of the decision tree 
     print("Score of fitted DT is {}".format(d_tree.score(X,y)))
@@ -87,8 +102,8 @@ def choose_best_dt(dt_list, mdp, n_iter=5_000):
     """Returns the decision tree with the best reward.
 
     Args:
-        dt_list (list): A list of decision trees
-        mdp (gym.Env): A Markov Decision Process (Reinforcement Learning Problem)
+        dt_list (list): A list of decision trees.
+        mdp (gym.Env): A Markov Decision Process (Reinforcement Learning Problem).
         n_iter (int): Number of exploration iterations. Defaults to 5_000.
 
     Returns:
@@ -120,28 +135,33 @@ def choose_best_dt(dt_list, mdp, n_iter=5_000):
     return best_tree
 
 
-def Viper(mdp, reward_weight, algo_dt, algo_rl, iter_viper, nb_data_from_nnpolicy, path_to_expert=None):
+def Viper(mdp, algo_dt, algo_rl, iter_viper, nb_data_from_nnpolicy, path_to_expert=None):
     """Proposes an implementation of Viper algorithm.
 
     Args:
-        mdp (gym.Env): A Markov Decision Process (Reinforcement Learning Problem)
-        reward_weight (float): Weight to control the influence of rewards on action selection
-        algo_dt : A decision tree algorithm
-        algo_rl : A RL Algorithm
-        iter_viper (int): Number of iterations for VIPER
-        nb_data_from_nnpolicy (int): Number of iterations for data generating functions
-        path_to_expert (str): a path to an existing policy. Defaults to None.
+        mdp (gym.Env): A Markov Decision Process (Reinforcement Learning Problem).
+        algo_dt : A decision tree algorithm.
+        algo_rl : A RL Algorithm.
+        iter_viper (int): Number of iterations for VIPER.
+        nb_data_from_nnpolicy (int): Number of iterations for data generating functions.
+        path_to_expert (str): A path to an existing policy. Defaults to None.
 
     Returns:
         The best decision tree to evaluate a policy.
     """
+    datasets = []
     policy = get_policy_nn(mdp, algo_rl, nb_data_from_nnpolicy, path_to_expert)
     dt_list = []
 
     for i in range(iter_viper):
         print('iteration {}'.format(i))
-        data = generate_data(mdp, reward_weight, policy, nb_data_from_nnpolicy)
-        dt = fit_dt(data, algo_dt)
+
+        data = generate_data(mdp, policy, nb_data_from_nnpolicy)
+
+        datasets.append(data)
+        dataset_dt = get_data_from_datasets(datasets)
+
+        dt = fit_dt(dataset_dt, algo_dt)
         dt_list.append(dt)
 
     return choose_best_dt(dt_list, mdp)
